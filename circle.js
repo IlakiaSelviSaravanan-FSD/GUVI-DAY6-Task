9501fb1c1ecd24b4d5760be3e61b44c082c8e41d
IlakiaSelviSaravanan-FSD <150881218+IlakiaSelviSaravanan-FSD@users.noreply.github.com>
2023-11-28 00:14:40 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -6,15 +6,22 @@ class Circle{
     }
 getCircumference(){
     return 2*Math.PI* this.radius;
+    
 }
 
 getArea(){
     return Math.PI * this.radius * this.radius;
+    
+}
+getColor(){
+    return `${this.color}`;
 }
 toString(){
-    return `this circle with radius $(this.radius),$(this.color) `;
+    return `this circle with radius $(this.radius)and color,$(this.color)` ;
 }
 }
-var c1= new Circle(8);
+var c1= new Circle(8,"red");
+
+console.log("the color of radius:"+c1.getColor());
 console.log("the value of area :"+c1.getArea().toFixed(2));
 console.log("the value of circumference:",+c1.getCircumference().toFixed(2));
